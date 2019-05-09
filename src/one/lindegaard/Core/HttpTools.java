0724c9cfe32d908f56b8f5247d170beb02b7a79a
Rocologo <rocologo@hotmail.com>
2019-05-09 18:49:27 +0200

    Made HttpTools to a Runnable

--- src/one/lindegaard/Core/HttpTools.java
+++ src/one/lindegaard/Core/HttpTools.java
@@ -5,6 +5,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.UnknownHostException;
 
+import org.bukkit.scheduler.BukkitRunnable;
+
 public class HttpTools {
 
 	public interface httpCallback {
@@ -14,48 +16,55 @@ public class HttpTools {
 	}
 
 	public static void isHomePageReachable(URL url, httpCallback callback) {
-		try {
-			// open a connection to that source
-			HttpURLConnection urlConnect = (HttpURLConnection) url.openConnection();
+		new BukkitRunnable() {
 
-			// trying to retrieve data from the source. If there
-			// is no connection, this line will fail
-			urlConnect.setConnectTimeout(5000);
-			urlConnect.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
-			urlConnect.addRequestProperty("User-Agent", "Mozilla");
-			urlConnect.addRequestProperty("Referer", "google.com");
+			@Override
+			public void run() {
 
-			boolean redirect = false;
+				try {
+					// open a connection to that source
+					HttpURLConnection urlConnect = (HttpURLConnection) url.openConnection();
 
-			// normally, 3xx is redirect
-			int status = urlConnect.getResponseCode();
-			if (status != HttpURLConnection.HTTP_OK) {
-				if (status == HttpURLConnection.HTTP_MOVED_TEMP || status == HttpURLConnection.HTTP_MOVED_PERM
-						|| status == HttpURLConnection.HTTP_SEE_OTHER)
-					redirect = true;
+					// trying to retrieve data from the source. If there
+					// is no connection, this line will fail
+					urlConnect.setConnectTimeout(5000);
+					urlConnect.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
+					urlConnect.addRequestProperty("User-Agent", "Mozilla");
+					urlConnect.addRequestProperty("Referer", "google.com");
+
+					boolean redirect = false;
+
+					// normally, 3xx is redirect
+					int status = urlConnect.getResponseCode();
+					if (status != HttpURLConnection.HTTP_OK) {
+						if (status == HttpURLConnection.HTTP_MOVED_TEMP || status == HttpURLConnection.HTTP_MOVED_PERM
+								|| status == HttpURLConnection.HTTP_SEE_OTHER)
+							redirect = true;
+					}
+
+					if (redirect) {
+
+						// get redirect url from "location" header field
+						String newUrl = urlConnect.getHeaderField("Location");
+
+						// open the new connnection again
+						urlConnect = (HttpURLConnection) new URL(newUrl).openConnection();
+
+						status = urlConnect.getResponseCode();
+
+					}
+					if (status == HttpURLConnection.HTTP_OK)
+						callback.onSuccess();
+					else
+						callback.onError();
+
+				} catch (UnknownHostException e) {
+					callback.onError();
+				} catch (IOException e) {
+					callback.onError();
+				}
 			}
-
-			if (redirect) {
-
-				// get redirect url from "location" header field
-				String newUrl = urlConnect.getHeaderField("Location");
-
-				// open the new connnection again
-				urlConnect = (HttpURLConnection) new URL(newUrl).openConnection();
-
-				status = urlConnect.getResponseCode();
-
-			}
-			if (status == HttpURLConnection.HTTP_OK)
-				callback.onSuccess();
-			else
-				callback.onError();
-
-		} catch (UnknownHostException e) {
-			callback.onError();
-		} catch (IOException e) {
-			callback.onError();
-		}
+		};
 	}
 
 }
